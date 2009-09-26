8992343a98d75df810fd9679b44663d7825d3165
katjae <katjae@artcom.de>
2009-09-26 14:03:14 +0200

    removed deprecated onResourceChanged

--- ResourceListener.java
+++ ResourceListener.java
@@ -4,9 +4,6 @@ import android.net.Uri;
 
 public interface ResourceListener {
 
-    @Deprecated
-    public void onResourceChanged(Uri pResourceUri);
-
     public void onResourceAvailable(Uri pResourceUri);
 
     public void onResourceNotAvailable(Uri pResourceUri);
