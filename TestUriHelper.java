f2b6c9c6f84821129522317014db9797a8ec0f71
Fabian Bützow (mobile) <fabian@artcom.de>
2010-02-18 16:46:14 +0100

    refactoring of httpproxyservice and cache

--- TestUriHelper.java
+++ TestUriHelper.java
@@ -5,18 +5,16 @@ import java.net.URISyntaxException;
 import android.net.Uri;
 
 public class TestUriHelper {
-    
+
     // Constants ---------------------------------------------------------
 
     public static final String BASE_URI = "http://www.artcom.de/templates/artcom/css/images/artcom_rgb_screen_193x22.png";
-    
-    
-    
+
     // Static Methods ----------------------------------------------------
 
     public static Uri createUri() throws URISyntaxException {
-        
-        return Uri.parse(BASE_URI+"?timestamp="+System.currentTimeMillis());
+
+        return Uri.parse(BASE_URI + "?timestamp=" + System.currentTimeMillis());
     }
 
 }
