c27856b180495a616a7ab87299043494a66d340c
Rodja Trappe <rodja@artcom.de>
2010-03-28 22:40:09 +0200

    MockedServer can now handle POST and GET ... the real testing can begin

--- MockHttpServer.java
+++ MockHttpServer.java
@@ -1,14 +1,17 @@
 package com.artcom.y60.http;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Properties;
+import java.util.UUID;
 
 import com.artcom.y60.Logger;
 
 public class MockHttpServer extends NanoHTTPD {
     
-    private static String LOG_TAG = "HttpServerForTesting";
-    private static int    PORT    = 4000;
+    private static String                     LOG_TAG          = "HttpServerForTesting";
+    private static int                        PORT             = 4000;
+    private final HashMap<String, Properties> mPostedResources = new HashMap<String, Properties>();
     
     public MockHttpServer() throws IOException {
         super(PORT);
@@ -17,8 +20,26 @@ public class MockHttpServer extends NanoHTTPD {
     @Override
     public Response serve(String uri, String method, Properties header, Properties parms) {
         Logger.v(LOG_TAG, method, " '", uri, "' ");
-        String msg = "I'm a mock server for test purposes";
-        return new NanoHTTPD.Response(HTTP_OK, MIME_HTML, msg);
+        
+        if (method.equals("GET")) {
+            String msg = null;
+            if (mPostedResources.containsKey(uri)) {
+                msg = mPostedResources.get(uri).getProperty("message");
+            } else {
+                msg = "I'm a mock server for test purposes";
+            }
+            return new NanoHTTPD.Response(HTTP_OK, MIME_PLAINTEXT, msg);
+        } else if (method.equals("POST")) {
+            String newResource = "/" + UUID.randomUUID();
+            mPostedResources.put(newResource, parms);
+            
+            NanoHTTPD.Response response = new NanoHTTPD.Response(HTTP_REDIRECT, MIME_PLAINTEXT,
+                    "see other: " + "http://localhost:" + PORT + newResource);
+            response.addHeader("Location", "http://localhost:" + PORT + newResource);
+            return response;
+        }
+        
+        return new NanoHTTPD.Response(HTTP_NOTIMPLEMENTED, MIME_PLAINTEXT, "not implemented");
     }
     
     public String getUri() {
--- TestMockedHttpServer.java
+++ TestMockedHttpServer.java
@@ -4,7 +4,7 @@ import java.net.SocketException;
 
 import com.artcom.y60.HttpHelper;
 
-public class TestOurMockedHttpServer extends HttpTestCase {
+public class TestMockedHttpServer extends HttpTestCase {
     
     public void testStartingAndStoppingTheServer() throws Exception {
         NanoHTTPD server = new NanoHTTPD(6066);
@@ -24,5 +24,10 @@ public class TestOurMockedHttpServer extends HttpTestCase {
     public void testHttpGet() throws Exception {
         assertEquals("our mock server should respond to GET",
                 "I'm a mock server for test purposes", HttpHelper.getAsString(getServer().getUri()));
+    }
+    
+    public void testHttpPost() throws Exception {
+        assertEquals("our mock server should respond to POST", "hello mock server", HttpHelper
+                .post(getServer().getUri(), "message=hello mock server", "txt/text", "txt/text"));
     }
 }
