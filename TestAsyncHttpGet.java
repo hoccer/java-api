2f3ceef54a3b96dd43a03d2225a6d463ed7aa9ec
Rodja Trappe <rodja@artcom.de>
2010-04-04 18:19:10 +0200

    added responseHandlerTests for AsyncHttpGet

--- TestAsyncHttpGet.java
+++ TestAsyncHttpGet.java
@@ -49,4 +49,50 @@ public class TestAsyncHttpGet extends HttpTestCase {
                     }
                 });
     }
+    
+    public void testGettingNoifiedAboutSuccessViaResponseHandler() throws Exception {
+        
+        getServer().setResponseDelay(200);
+        mHttpGet = new AsyncHttpGet(getServer().getUri());
+        ResponseHandlerForTesting requestStatus = new ResponseHandlerForTesting();
+        mHttpGet.registerResponseHandler(requestStatus);
+        mHttpGet.start();
+        Thread.sleep(50);
+        assertTrue("request should have started, but is at " + mHttpGet.getProgress() + "%",
+                mHttpGet.isRunning());
+        assertTrue("should be connecting", requestStatus.isConnecting);
+        TestHelper.blockUntilTrue("request should have been performed by now", 3000,
+                new TestHelper.Condition() {
+                    
+                    @Override
+                    public boolean isSatisfied() throws Exception {
+                        return mHttpGet.isDone();
+                    }
+                });
+        assertTrue("should be successful", requestStatus.wasSuccessful);
+        assertNotNull("should have an response body", requestStatus.body);
+        assertEquals("response should come from mocked server",
+                "I'm a mock server for test purposes", requestStatus.body.toString());
+    }
+    
+    public void testGettingNoifiedAboutFailureViaResponseHandler() throws Exception {
+        
+        mHttpGet = new AsyncHttpGet(getServer().getUri() + "/not-a-valid-address");
+        final ResponseHandlerForTesting requestStatus = new ResponseHandlerForTesting();
+        mHttpGet.registerResponseHandler(requestStatus);
+        mHttpGet.start();
+        TestHelper.blockUntilTrue("request should have called onError", 2000,
+                new TestHelper.Condition() {
+                    
+                    @Override
+                    public boolean isSatisfied() throws Exception {
+                        return requestStatus.hasError;
+                    }
+                });
+        assertFalse("should not be successful", requestStatus.wasSuccessful);
+        assertNotNull("should have no response body", requestStatus.body);
+        assertEquals("response from mocked server should describe 404", "Not Found",
+                requestStatus.body.toString());
+    }
+    
 }
