4de598c2d3a710ab30d56abe731a14effc09a499
Fabian Bützow (mobile) <fabian@artcom.de>
2010-04-19 17:19:28 +0200

    header mine info and body combined in StreamableContent

--- StreamableString.java
+++ StreamableString.java
@@ -3,22 +3,27 @@ package com.artcom.y60.data;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-public class StreamableString implements Streamable {
-    
+public class StreamableString implements StreamableContent {
+
     byte[] mData;
-    
+
     public StreamableString(String text) {
         mData = text.getBytes();
     }
-    
+
     @Override
     public InputStream getStream() {
         return new ByteArrayInputStream(mData);
     }
-    
+
     @Override
     public long getStreamLength() {
         return mData.length;
     }
-    
+
+    @Override
+    public String getContentType() {
+        return "text/plain";
+    }
+
 }
