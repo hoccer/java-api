159399129187702fa31b530cc717607394e962df
Rodja Trappe <rodja@artcom.de>
2010-04-22 09:00:31 +0200

    forgot to add exception

---
+++ UnknownContentTypeException.java
@@ -0,0 +1,11 @@
+package com.artcom.y60.data;
+
+public class UnknownContentTypeException extends Exception {
+    
+    private static final long serialVersionUID = 159123072833046461L;
+    
+    public UnknownContentTypeException(String message) {
+        super(message);
+    }
+    
+}
