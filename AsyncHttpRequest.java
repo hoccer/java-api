f1e2b905fd35cf43b6b5601777b4b789d9b5d8c2
Rodja Trappe <rodja@artcom.de>
2010-04-25 08:31:17 +0200

    seems like the server presents the hoccer 'uploads' field befor content is available

--- AsyncHttpRequest.java
+++ AsyncHttpRequest.java
@@ -218,7 +218,7 @@ public abstract class AsyncHttpRequest extends ThreadedTask {
     
     protected void onHttpHeaderAvailable(Header[] pHeaders) {
         
-        // Logger.v(LOG_TAG, "headers: ", HttpHelper.getHeadersAsString(pHeaders));
+        Logger.v(LOG_TAG, "headers of ", getUri(), ": ", HttpHelper.getHeadersAsString(pHeaders));
         
         HashMap<String, String> headers = new HashMap<String, String>();
         for (int i = 0; i < pHeaders.length; i++) {
