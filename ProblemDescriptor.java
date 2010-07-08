fbb73f543c4032bc612ae2c43d005f1a13be36a1
Fabian Bützow (mobile) <fabian@artcom.de>
2010-07-08 16:03:19 +0200

    constants in problem descriptor

--- ProblemDescriptor.java
+++ ProblemDescriptor.java
@@ -1,5 +1,19 @@
 package com.artcom.y60.data;
 
 public interface ProblemDescriptor {
+
+    public static final String HOCCABILITY_INTRO     = "hoccability_intro";
+    public static final String HOCCABILITY_HINT_JOIN = "hoccability_tip_join";
+
+    public static class Problems {
+        public static final String HOCCABILITY_BAD     = "hoccability_bad";
+        public static final String HOCCABILITY_PERFECT = "hoccability_perfect";
+    }
+
+    public static class Suggestions {
+        public static final String HOCCABILITY_TURN_WIFI_ON = "hoccability_improve_turn_wifi_on";
+        public static final String HOCCABILITY_GO_OUTSIDE   = "hoccability_improve_go_outside";
+    }
+
     public String getDescription(String id);
 }
