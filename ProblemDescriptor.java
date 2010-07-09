442ed9659c4e161db66477ba5a4d468a12350847
Fabian Bützow (mobile) <fabian@artcom.de>
2010-07-09 09:55:23 +0200

    hoccability description strings

--- ProblemDescriptor.java
+++ ProblemDescriptor.java
@@ -2,17 +2,24 @@ package com.artcom.y60.data;
 
 public interface ProblemDescriptor {
 
-    public static final String HOCCABILITY_INTRO     = "hoccability_intro";
-    public static final String HOCCABILITY_HINT_JOIN = "hoccability_tip_join";
-
     public static class Problems {
-        public static final String HOCCABILITY_BAD     = "hoccability_bad";
-        public static final String HOCCABILITY_PERFECT = "hoccability_perfect";
+        public static final String HOCCABILITY_BAD  = "hoccability_bad";
+        public static final String HOCCABILITY_OK   = "hoccability_ok";
+        public static final String HOCCABILITY_GOOD = "hoccability_good";
     }
 
     public static class Suggestions {
-        public static final String HOCCABILITY_TURN_WIFI_ON = "hoccability_improve_turn_wifi_on";
-        public static final String HOCCABILITY_GO_OUTSIDE   = "hoccability_improve_go_outside";
+
+        public static final String HOCCABILITY_0                     = "hoccability_0";
+
+        public static final String HOCCABILITY_1_GPS_BAD_BSSIDS_GOOD = "hoccability_1_gps_bad_bssids_good";
+        public static final String HOCCABILITY_1_GPS_OK_BSSIDS_BAD   = "hoccability_1_gps_ok_bssids_bad";
+
+        public static final String HOCCABILITY_2_GPS_OK_BSSIDS_GOOD  = "hoccability_2_gps_ok_bssids_good";
+        public static final String HOCCABILITY_2_GPS_GOOD_BSSIDS_BAD = "hoccability_2_gps_good_bssids_bad";
+
+        public static final String HOCCABILITY_3                     = "hoccability_3";
+
     }
 
     public String getDescription(String id);
