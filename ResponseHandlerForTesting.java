d2e55391c6e838d02d1a8f6bbfc25fbdd5815c46
Rodja Trappe <mail@rodja.net>
2010-07-28 11:50:57 +0200

    fixed unit tests

--- ResponseHandlerForTesting.java
+++ ResponseHandlerForTesting.java
@@ -6,18 +6,18 @@ import com.artcom.y60.Logger;
 import com.artcom.y60.data.StreamableContent;
 
 public class ResponseHandlerForTesting implements HttpResponseHandler {
-    
+
     private static final String LOG_TAG                         = "ResponseHandlerForTesting";
-    
+
     boolean                     areHeadersAvailable             = false;
     boolean                     hasOnHeadersAvailableBeenCalled = false;
     boolean                     hasError                        = false;
     boolean                     isReceiving                     = false;
     boolean                     wasSuccessful                   = false;
-    
+
     double                      progress                        = -1;
     StreamableContent           body                            = null;
-    
+
     @Override
     public void onHeaderAvailable(HashMap<String, String> headers) {
         Logger.v(LOG_TAG, "onHeaderAvailable called");
@@ -25,7 +25,7 @@ public class ResponseHandlerForTesting implements HttpResponseHandler {
         areHeadersAvailable = true;
         hasOnHeadersAvailableBeenCalled = true;
     }
-    
+
     @Override
     public void onError(int statusCode, StreamableContent body) {
         Logger.v(LOG_TAG, "onError called");
@@ -33,15 +33,13 @@ public class ResponseHandlerForTesting implements HttpResponseHandler {
         hasError = true;
         this.body = body;
     }
-    
+
     @Override
     public void onReceiving(double pProgress) {
         Logger.v(LOG_TAG, "onReceiving called");
-        reset();
-        isReceiving = true;
         progress = pProgress;
     }
-    
+
     @Override
     public void onSuccess(int statusCode, StreamableContent body) {
         Logger.v(LOG_TAG, "onSuccess called");
@@ -49,15 +47,15 @@ public class ResponseHandlerForTesting implements HttpResponseHandler {
         wasSuccessful = true;
         this.body = body;
     }
-    
+
     void reset() {
         areHeadersAvailable = hasError = isReceiving = wasSuccessful = false;
         body = null;
     }
-    
+
     @Override
     public void onError(Exception e) {
         hasError = true;
     }
-    
+
 }
