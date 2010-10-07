70bf5f21f7c5af30c76cb851fcb906d227734045
Rodja Trappe <rodja@hoccer.com>
2010-10-07 14:05:36 +0200

    remaming test class

--- tests/com/hoccer/api/TestRESTfulApi.java
+++ tests/com/hoccer/api/TestRESTfulApi.java
@@ -7,7 +7,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-public class TestRESTfulAPI extends TestCase {
+public class TestRESTfulApi extends TestCase {
 
     DefaultHttpClient mHttpClient;
 
