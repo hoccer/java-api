652a5d7c5e579375151c5fc07398852587782c97
Rodja Trappe <rodja@hoccer.com>
2010-10-27 13:22:13 +0200

    started with one-to-many test

--- tests/com/hoccer/api/TestLinccing.java
+++ tests/com/hoccer/api/TestLinccing.java
@@ -19,7 +19,7 @@ public class TestLinccing {
         linccerA.onGpsChanged(22.012, 102.115, 130);
         linccerB.onGpsChanged(22.012, 102.11, 1030);
 
-        ThreadedShare threadedShare = new ThreadedShare(linccerA);
+        ThreadedShare threadedShare = new ThreadedShare(linccerA, "1:1");
         threadedShare.start();
 
         JSONObject receivedPayload = linccerB.receive("1:1");
@@ -43,9 +43,9 @@ public class TestLinccing {
         linccerB.onGpsChanged(22.011, 102.11, 1030);
         linccerC.onGpsChanged(22.009, 102.116, 2000);
 
-        ThreadedShare threadedShare = new ThreadedShare(linccerA);
+        ThreadedShare threadedShare = new ThreadedShare(linccerA, "1:1");
         threadedShare.start();
-        ThreadedReceive threadedReceive = new ThreadedReceive(linccerB);
+        ThreadedReceive threadedReceive = new ThreadedReceive(linccerB, "1:1");
         threadedReceive.start();
 
         JSONObject receivedPayload = linccerC.receive("1:1");
@@ -62,13 +62,46 @@ public class TestLinccing {
         assertNotNull("should have got status object", threadedReceive.getResult());
     }
 
-    class ThreadedLinccing extends Thread {
-        protected Exception   mException;
-        private final Linccer mLinccer;
-        JSONObject            mResult;
+    @Test(timeout = 6000)
+    public void oneToManySuccess() throws Exception {
+        final Linccer linccerA = new Linccer(createDescription());
+        final Linccer linccerB = new Linccer(createDescription());
+        final Linccer linccerC = new Linccer(createDescription());
 
-        public ThreadedLinccing(Linccer linccer) {
+        linccerA.onGpsChanged(22.012, 102.113, 130);
+        linccerB.onGpsChanged(22.011, 102.11, 1030);
+        linccerC.onGpsChanged(22.009, 102.116, 2000);
+
+        ThreadedShare threadedShare = new ThreadedShare(linccerA, "1:n");
+        threadedShare.start();
+        ThreadedReceive threadedReceive = new ThreadedReceive(linccerB, "1:n");
+        threadedReceive.start();
+
+        JSONObject receivedPayload = linccerC.receive("1:n");
+        assertNotNull("should have received something", receivedPayload);
+        assertTrue("should have received a message", receivedPayload.has("message"));
+        assertEquals("hello world", receivedPayload.get("message"));
+
+        threadedShare.join();
+        threadedShare.assertNoExceptionsOccured();
+        assertEquals("should have got the payload", receivedPayload.toString(), threadedShare
+                .getResult().toString());
+
+        threadedReceive.join();
+        threadedReceive.assertNoExceptionsOccured();
+        assertEquals("should also have got the payload", receivedPayload.toString(),
+                threadedReceive.getResult().toString());
+    }
+
+    class ThreadedLinccing extends Thread {
+        protected Exception    mException;
+        private final Linccer  mLinccer;
+        JSONObject             mResult;
+        protected final String mMode;
+
+        public ThreadedLinccing(Linccer linccer, String mode) {
             mLinccer = linccer;
+            mMode = mode;
         }
 
         public Linccer getLinccer() {
@@ -88,8 +121,8 @@ public class TestLinccing {
 
     class ThreadedShare extends ThreadedLinccing {
 
-        public ThreadedShare(Linccer linccer) {
-            super(linccer);
+        public ThreadedShare(Linccer linccer, String mode) {
+            super(linccer, mode);
         }
 
         @Override
@@ -97,7 +130,7 @@ public class TestLinccing {
             try {
                 JSONObject payload = new JSONObject();
                 payload.put("message", "hello world");
-                mResult = getLinccer().share("1:1", payload);
+                mResult = getLinccer().share(mMode, payload);
             } catch (Exception e) {
                 mException = e;
             }
@@ -106,14 +139,14 @@ public class TestLinccing {
 
     class ThreadedReceive extends ThreadedLinccing {
 
-        public ThreadedReceive(Linccer linccer) {
-            super(linccer);
+        public ThreadedReceive(Linccer linccer, String mode) {
+            super(linccer, mode);
         }
 
         @Override
         public void run() {
             try {
-                mResult = getLinccer().receive("1:1");
+                mResult = getLinccer().receive(mMode);
             } catch (Exception e) {
                 mException = e;
             }
