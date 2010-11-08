690dd4f06e516f1ad5c4d73ea7c39263400cfbb9
Rodja Trappe <rodja@hoccer.com>
2010-11-08 15:06:32 +0100

    added 'not matching' tests, but server does not work properly, so they fail

--- tests/com/hoccer/api/TestEnvironment.java
+++ tests/com/hoccer/api/TestEnvironment.java
@@ -19,6 +19,17 @@ public class TestEnvironment extends LinccerTest {
     }
 
     @Test(timeout = 20000)
+    public void gpsNotMatchingTest() throws Exception {
+        final Linccer linccerA = new Linccer(createDescription());
+        Linccer linccerB = new Linccer(createDescription());
+
+        linccerA.onGpsChanged(24.012, 102.115, 130);
+        linccerB.onGpsChanged(22.012, 102.11, 1030);
+
+        assertNotConnectable(linccerA, linccerB);
+    }
+
+    @Test(timeout = 20000)
     public void networkTest() throws Exception {
         final Linccer linccerA = new Linccer(createDescription());
         Linccer linccerB = new Linccer(createDescription());
@@ -30,6 +41,17 @@ public class TestEnvironment extends LinccerTest {
     }
 
     @Test(timeout = 20000)
+    public void networkNotMatchingTest() throws Exception {
+        final Linccer linccerA = new Linccer(createDescription());
+        Linccer linccerB = new Linccer(createDescription());
+
+        linccerA.onNetworkChanged(24.012, 102.115, 130);
+        linccerB.onNetworkChanged(22.012, 102.11, 1030);
+
+        assertNotConnectable(linccerA, linccerB);
+    }
+
+    @Test(timeout = 20000)
     public void wifiTest() throws Exception {
         final Linccer linccerA = new Linccer(createDescription());
         Linccer linccerB = new Linccer(createDescription());
@@ -38,6 +60,17 @@ public class TestEnvironment extends LinccerTest {
         linccerB.onWifiChanged(new String[] { "00:22:3F:11:5A:2E", "5C:12:3F:11:5A:2C" });
 
         assertConnectable(linccerA, linccerB);
+    }
+
+    @Test(timeout = 20000)
+    public void wifiNotMatchingTest() throws Exception {
+        final Linccer linccerA = new Linccer(createDescription());
+        Linccer linccerB = new Linccer(createDescription());
+
+        linccerA.onWifiChanged(new String[] { "00:22:3F:11:5A:2E", "4C:12:3F:11:5A:2C" });
+        linccerB.onWifiChanged(new String[] { "10:22:3F:11:5A:2E", "5C:12:3F:11:5A:2C" });
+
+        assertNotConnectable(linccerA, linccerB);
     }
 
     private void assertConnectable(final Linccer linccerA, Linccer linccerB)
@@ -55,5 +88,19 @@ public class TestEnvironment extends LinccerTest {
         threadedShare.assertNoExceptionsOccured();
         assertNotNull("should have shared the message", threadedShare.getResult());
         assertEquals("hello world", threadedShare.getResult().get("message"));
+    }
+
+    private void assertNotConnectable(final Linccer linccerA, Linccer linccerB)
+            throws BadModeException, ClientActionException, CollidingActionsException,
+            JSONException, InterruptedException, Exception {
+        ThreadedShare threadedShare = new ThreadedShare(linccerA, "1:1");
+        threadedShare.start();
+
+        JSONObject receivedPayload = linccerB.receive("1:1");
+        assertNull("should not have received something", receivedPayload);
+
+        threadedShare.join();
+        threadedShare.assertNoExceptionsOccured();
+        assertNull("should not have shared the message", threadedShare.getResult());
     }
 }
