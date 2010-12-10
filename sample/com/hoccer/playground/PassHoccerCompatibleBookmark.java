fac67186589a4fb32979615baf67e390196f87e2
Rodja Trappe <rodja@hoccer.com>
2010-12-10 13:30:17 +0100

    sharing image instead of bookmark

--- sample/com/hoccer/playground/PassHoccerCompatibleBookmark.java
+++ sample/com/hoccer/playground/PassHoccerCompatibleBookmark.java
@@ -10,6 +10,9 @@ public class PassHoccerCompatibleBookmark {
 
     public static void main(String[] args) {
 
+        // String content = "{type: 'text/uri-list', content: 'http://hoccer.com/'}";
+        String content = "{type: 'image/jpeg', uri: 'http://hoccer.com/wp-content/themes/hoccer/images/logo.jpg'}";
+
         try {
             final Linccer linccer = new Linccer(new ClientConfig("Simple Hoccer Bookmark Passer"));
             linccer.onGpsChanged(52.5167780325, 13.409039925, 10000);
@@ -17,8 +20,7 @@ public class PassHoccerCompatibleBookmark {
             JSONObject payload = new JSONObject();
             payload.put("sender", new JSONObject("{client-id: '"
                     + linccer.getClientConfig().getClientId() + "'}"));
-            payload.put("data", new JSONArray(
-                    "[{type: 'text/uri-list', content: 'http://hoccer.com/'}]"));
+            payload.put("data", new JSONArray("[" + content + "]"));
 
             JSONObject result = linccer.share("1:n", payload);
             if (result == null)
