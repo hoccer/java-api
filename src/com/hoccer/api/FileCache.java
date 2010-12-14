a1230f9fbcd050ecc1236f8c68617a42dd5f013a
Rodja Trappe <rodja@hoccer.com>
2010-12-14 16:29:53 +0100

    cleanup

--- src/com/hoccer/api/FileCache.java
+++ src/com/hoccer/api/FileCache.java
@@ -3,6 +3,7 @@ package com.hoccer.api;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.UUID;
 
 import org.apache.http.HttpResponse;
@@ -13,18 +14,24 @@ import org.apache.http.client.methods.HttpPost;
 
 import com.hoccer.data.StreamableContent;
 import com.hoccer.http.AsyncHttpPut;
+import com.hoccer.http.AsyncHttpRequest;
 import com.hoccer.http.HttpResponseHandler;
 import com.hoccer.http.MultipartHttpEntity;
-import com.hoccer.thread.StatusHandler;
 
 public class FileCache extends CloudService {
 
-    private long    downloadTime;
-    private boolean isFetchStopped;
-    private int     progress;
+    private long                                    downloadTime;
+    private boolean                                 isFetchStopped;
+    private int                                     progress;
+
+    private final HashMap<String, AsyncHttpRequest> mOngoingRequests = new HashMap<String, AsyncHttpRequest>();
 
     public FileCache(ClientConfig config) {
         super(config);
+    }
+
+    public HashMap<String, AsyncHttpRequest> getOngoingRequests() {
+        return mOngoingRequests;
     }
 
     public String store(StreamableContent data, int secondsUntilExipred)
@@ -32,22 +39,6 @@ public class FileCache extends CloudService {
 
         MultipartHttpEntity multipart = new MultipartHttpEntity();
         multipart.addPart("upload", data);
-        multipart.registerStatusHandler(new StatusHandler() {
-
-            @Override
-            public void onSuccess() {
-            }
-
-            @Override
-            public void onProgress(int progress) {
-                FileCache.this.progress = progress;
-            }
-
-            @Override
-            public void onError(Throwable e) {
-            }
-        });
-
         String url = ClientConfig.getFileCacheBaseUri() + "/?expires_in=" + secondsUntilExipred;
         HttpPost request = new HttpPost(sign(url));
         request.setEntity(multipart);
@@ -58,8 +49,6 @@ public class FileCache extends CloudService {
 
     public void fetch(String locationUri, StreamableContent data) throws ClientProtocolException,
             IOException {
-        isFetchStopped = false;
-
         HttpGet request = new HttpGet(locationUri);
         HttpResponse response = getHttpClient().execute(request);
 
@@ -71,33 +60,14 @@ public class FileCache extends CloudService {
 
         InputStream is = response.getEntity().getContent();
         OutputStream storageStream = data.openOutputStream();
-        long downloaded = 0;
-        long size = response.getEntity().getContentLength();
         byte[] buffer = new byte[0xFFFF];
         int len;
-        long downloadStart = System.currentTimeMillis();
         while ((len = is.read(buffer)) != -1) {
             if (isFetchStopped) {
                 return;
             }
-
-            setProgress((int) ((downloaded / (double) size) * 100));
             storageStream.write(buffer, 0, len);
-            downloaded += len;
         }
-        downloadTime = System.currentTimeMillis() - downloadStart;
-    }
-
-    private void setProgress(int percent) {
-        this.progress = percent;
-    }
-
-    public int getProgress() {
-        return progress;
-    }
-
-    public void stopFetch() {
-        isFetchStopped = true;
     }
 
     public String asyncStore(StreamableContent data, int secondsUntilExipred,
@@ -111,5 +81,10 @@ public class FileCache extends CloudService {
         storeRequest.start();
 
         return uri;
+    }
+
+    public void cancel(String uri) {
+        AsyncHttpRequest request = mOngoingRequests.remove(uri);
+        request.interrupt();
     }
 }
